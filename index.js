b9abb34fec32ad74c063f0737440bb90f8a42bf4
Marcelo Martinez <marcelomst1@gmail.com>
2019-12-31 19:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ function takeANumber(katzDeliLine, personName){
   katzDeliLine.push(personName);
   return `Welcome, ${personName}. You are number ${katzDeliLine.length} in line.`
  }
- 
+ function nowServing(katzDeliLine){
+   if (katzDeliLine.length === 0){
+     return 
+   }
+ }
  
